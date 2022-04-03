0c7c7f6570db4683a93a99d64d2a6ff85e841215
Andrey Tyurin <ectb08@mail.ru>
2022-04-03 19:18:27 +0300

    update github page config

--- test/app/vue.config.js
+++ test/app/vue.config.js
@@ -8,8 +8,8 @@ const aliasObj = {
 };
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/vue-dialog/',
-  outputDir: process.env.NODE_ENV === 'production' ? __dirname + '/lib-build' : __dirname + '/demo',
+  // publicPath: process.env.NODE_ENV === 'production' ? '/' : '/vue-dialog/',
+  // outputDir: process.env.NODE_ENV === 'production' ? __dirname + '/lib-build' : __dirname + '/demo',
   css: {
     extract: false
   },
