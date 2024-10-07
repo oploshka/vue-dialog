8562f4bb0eddc2852f4c3ee3a0f68063743e7567
Andrey Tyurin <ectb08@mail.ru>
2024-10-07 17:20:24 +0300

    wip update

--- example/plugin-install/Group/Wrapper/DlgGroupModal.vue
+++ example/plugin-install/Group/Wrapper/DlgGroupModal.vue
@@ -1,7 +1,7 @@
 <template>
   <TransitionGroup tag="div" class="dlg-group--modal" appear mode="out-in" name="bounce">
     <template v-for="modal in modalList" :key="modal.getId()">
-      <DlgGroupModalItem :theme="modal.getTheme()">
+      <DlgGroupModalItem :theme="modal.getTheme()" class="dlg-item">
         <component
             :is="modal.getVueComponent()"
             v-bind="modal.getVueComponentProps()"
@@ -51,13 +51,30 @@ export default {
   bottom: 0;
   z-index: 2100; // для перекрытия бутстраповского header fixed
 
-  // for dlg-item
+
+  overflow: unset;
+  opacity: 1;
+
   display: flex;
-  flex-direction: column;
-  flex-wrap: nowrap;
-  gap: 16px;
-  //
+  display: -ms-flexbox;
+  align-items: center;
+  -ms-flex-align: center;
+  -ms-flex-pack: center;
   justify-content: center;
+  
   padding: 25px;
+
+  //
+  .dlg-item {
+    max-width: 940px;
+    width: auto;
+    //position: relative
+  }
+
+  .dlg-item.dlg-item__max-size {
+    max-width: none;
+    width: 100%;
+  }
 }
+
 </style>
