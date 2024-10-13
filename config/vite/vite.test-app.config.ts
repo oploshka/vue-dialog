adbf9028a51e5a429f90d0f2f72b938b65050db8
Andrey Tyurin <ectb08@mail.ru>
2024-10-13 21:50:31 +0300

    wip update webpack to vite

---
+++ config/vite/vite.test-app.config.ts
@@ -0,0 +1,12 @@
+//
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+
+console.log('test-app-serve');
+
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+})
