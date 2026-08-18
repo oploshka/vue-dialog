e8a878f6516597d71549ea4f8b2d2c8388d0e750
oploshka <43810171+oploshka@users.noreply.github.com>
2026-08-19 02:10:54 +0300

    Add modal layout scaffold

---
+++ src/v3/research/Layer/Modal/ModalLayout.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="modal-layout" :style="{ zIndex: manager.zIndex }">
+    <TransitionGroup name="modal-stack">
+      <div
+        v-for="item in manager.items"
+        :key="item.id"
+        class="modal-element"
+        :style="{ zIndex: item.zIndex }"
+      >
+        <component
+          :is="item.settings.presenterComp ?? ModalPresenter"
+          v-bind="getPresenterProps(item)"
+        />
+      </div>
+    </TransitionGroup>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ModalPresenter from '../../Presenter/ModalPresenter.vue'
+import type { Modal } from './Modal'
+import type { ModalController } from './ModalController'
+import { setModalComponentRef } from './ModalRuntime'
+
+defineProps<{ manager: ModalController }>()
+
+function getPresenterProps(modal: Modal) {
+  return {
+    component: modal.component,
+    componentProps: modal.props,
+
+    wrapComp: modal.settings.wrapComp,
+    wrapProps: modal.settings.wrapProps ?? {},
+
+    bridgeComp: modal.settings.bridgeComp,
+    bridgeProps: modal.settings.bridgeProps ?? {},
+
+    settings: modal.settings.presenterProps ?? {},
+
+    closeOnBackdrop: modal.settings.closeOnBackdrop,
+    close: () => modal.close(),
+    bindComponentRef: (componentRef: unknown) => setModalComponentRef(modal, componentRef),
+  }
+}
+</script>
+
+<style scoped>
+.modal-layout,
+.modal-element {
+  position: fixed;
+  inset: 0;
+}
+</style>
