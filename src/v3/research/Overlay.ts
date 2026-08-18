25a68c2219eefeab136691ec01c9f4d3f2e68727
oploshka <43810171+oploshka@users.noreply.github.com>
2026-08-19 02:11:00 +0300

    Use modal layout in v3 overlay host

--- src/v3/research/Overlay.ts
+++ src/v3/research/Overlay.ts
@@ -1,42 +1,21 @@
-// Overlays.ts
-
 import { ModalController } from './Layer/Modal/ModalController'
 import { NotifyController } from './Layer/Notify/NotifyController'
-import ModalLayer from './Layer/Modal/ModalLayer.vue'
+import ModalLayout from './Layer/Modal/ModalLayout.vue'
 import NotifyLayer from './Layer/Notify/NotifyLayer.vue'
-import { resolveWrapper } from './WrapperRegistry'
-import type { sWrapperConfig } from './Type/Type'
 
 const modalController = new ModalController(3000)
 const notifyController = new NotifyController(5000)
 
-// ----------------------------------------------------------
-// Кастомные врапперы
-// ----------------------------------------------------------
-const customWrappers: Record<string, sWrapperConfig> = {}
-
-export function getWrapper(type: string): sWrapperConfig | undefined {
-  return customWrappers[type] ?? resolveWrapper(type)
-}
-
-// ----------------------------------------------------------
-// popstate
-// ----------------------------------------------------------
 window.addEventListener('popstate', () => {
-  if (modalController.items.length > 0) {
-    modalController.close()
-  }
+  modalController.top?.close()
 })
 
-// ----------------------------------------------------------
-// Экспорт
-// ----------------------------------------------------------
 export const overlays = {
   modal: modalController,
   notify: notifyController,
 }
 
 export const layerConfig = [
-  { manager: modalController, template: ModalLayer },
+  { manager: modalController, template: ModalLayout },
   { manager: notifyController, template: NotifyLayer },
 ]
