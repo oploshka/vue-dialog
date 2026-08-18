92e0cd4f8b1ce27c5703b246fca4dbb4b037048e
oploshka <43810171+oploshka@users.noreply.github.com>
2026-08-19 02:10:31 +0300

    Add default component bridge

---
+++ src/v3/research/Bridge/DefaultBridge.vue
@@ -0,0 +1,19 @@
+<template>
+  <component
+    :is="component"
+    v-bind="componentProps"
+    :ref="bindComponentRef"
+  />
+</template>
+
+<script setup lang="ts">
+import type { Component } from 'vue'
+import type { tProps } from '../Type/Type'
+
+defineProps<{
+  component: Component
+  componentProps: tProps
+  close: () => void
+  bindComponentRef: (componentRef: unknown) => void
+}>()
+</script>
