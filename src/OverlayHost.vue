aac530f3968570f73e7a115ef0bbbb17b760054a
oploshka <43810171+oploshka@users.noreply.github.com>
2026-08-19 02:55:52 +0300

    Use Options API in overlay host

--- src/OverlayHost.vue
+++ src/OverlayHost.vue
@@ -2,46 +2,61 @@
 <template>
   <div @keydown.esc="handleEsc">
     <component
-        v-for="entry in sortedLayers"
-        :key="entry.manager.id"
-        :is="entry.template"
-        :manager="entry.manager"
+      v-for="entry in sortedLayers"
+      :key="entry.manager.id"
+      :is="entry.template"
+      :manager="entry.manager"
     />
   </div>
 </template>
 
-<script setup lang="ts">
-import { computed, watch } from 'vue'
-import type { Component } from 'vue'
+<script lang="ts">
+import { defineComponent, type Component, type PropType } from 'vue'
 import type { sLayerController } from './Type/Type'
 import { lockBodyScroll, unlockBodyScroll } from './Utils/BodyScroll'
 
-const props = defineProps<{
-  layers: Array<{
-    manager: sLayerController
-    template: Component
-  }>
-}>()
+type tLayerEntry = {
+  manager: sLayerController
+  template: Component
+}
 
-const sortedLayers = computed(() =>
-    [...props.layers].sort((a, b) => a.manager.zIndex - b.manager.zIndex)
-)
+export default defineComponent({
+  name: 'OverlayHost',
 
-watch(
-    () => props.layers.some(l => l.manager.items.length > 0),
-    (hasItems) => {
-      if (hasItems) {
+  props: {
+    layers: {
+      type: Array as PropType<tLayerEntry[]>,
+      required: true,
+    },
+  },
+
+  computed: {
+    sortedLayers(): tLayerEntry[] {
+      return [...this.layers].sort((a, b) => a.manager.zIndex - b.manager.zIndex)
+    },
+
+    hasItems(): boolean {
+      return this.layers.some(layer => layer.manager.items.length > 0)
+    },
+  },
+
+  watch: {
+    hasItems(value: boolean): void {
+      if (value) {
         lockBodyScroll('modal')
       } else {
         unlockBodyScroll('modal')
       }
-    }
-)
+    },
+  },
 
-function handleEsc() {
-  const reversed = [...sortedLayers.value].reverse()
-  for (const entry of reversed) {
-    if (entry.manager.handleEsc?.()) return
-  }
-}
+  methods: {
+    handleEsc(): void {
+      const reversed = [...this.sortedLayers].reverse()
+      for (const entry of reversed) {
+        if (entry.manager.handleEsc?.()) return
+      }
+    },
+  },
+})
 </script>
