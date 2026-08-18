ec0d0c3ed09eb821d08dc07dab16e867421dc4bd
oploshka <43810171+oploshka@users.noreply.github.com>
2026-08-19 02:10:06 +0300

    Simplify v3 Modal runtime object

--- src/v3/research/Layer/Modal/Modal.ts
+++ src/v3/research/Layer/Modal/Modal.ts
@@ -1,42 +1,30 @@
 import type { Component } from 'vue'
-import type { sLayerDescriptor } from '../../Type/Type'
-
-type tModalSettings = sLayerDescriptor['settings']
-type tModalCallbacks = sLayerDescriptor['callbacks']
+import type { sLayerDescriptor, sModalSettings, tProps } from '../../Type/Type'
+import { getModalComponentRef } from './ModalRuntime'
 
 type tModalConfig = {
   id: string
   zIndex: number
-  type: string
-  variant?: string
   component: Component
-  props: Record<string, any>
-  settings: tModalSettings
-  callbacks: tModalCallbacks
+  props: tProps
+  settings: sModalSettings
 }
 
 export class Modal implements sLayerDescriptor {
   readonly id: string
   readonly zIndex: number
-  readonly type: string
-  readonly variant?: string
   readonly component: Component
-  readonly props: Record<string, any>
-  readonly settings: tModalSettings
-  readonly callbacks: tModalCallbacks
+  readonly props: tProps
+  readonly settings: sModalSettings
 
-  private componentRef: unknown = null
   private readonly requestClose: (modal: Modal) => void
 
   constructor(config: tModalConfig, requestClose: (modal: Modal) => void) {
     this.id = config.id
     this.zIndex = config.zIndex
-    this.type = config.type
-    this.variant = config.variant
     this.component = config.component
     this.props = config.props
     this.settings = config.settings
-    this.callbacks = config.callbacks
     this.requestClose = requestClose
   }
 
@@ -46,10 +34,6 @@ export class Modal implements sLayerDescriptor {
   }
 
   getComponentRef<T = unknown>(): T | null {
-    return this.componentRef as T | null
-  }
-
-  setComponentRef(ref: unknown): void {
-    this.componentRef = ref
+    return getModalComponentRef<T>(this)
   }
 }
