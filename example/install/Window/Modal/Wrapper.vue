7e9dedaaa963a20eb278d0853ceefe58f9a5bbb7
oploshka <43810171+oploshka@users.noreply.github.com>
2026-08-19 22:57:55 +0300

    Restore modal wrapper padding

--- example/install/Window/Modal/Wrapper.vue
+++ example/install/Window/Modal/Wrapper.vue
@@ -45,6 +45,7 @@ export default defineComponent({
   box-sizing: border-box;
   max-height: 90vh;
   overflow: auto;
+  padding: 24px;
   background: #fff;
   border-radius: 8px;
   box-shadow: 0 16px 40px rgba(28, 41, 61, 0.16);
