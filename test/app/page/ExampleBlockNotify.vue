0f7b08186239e10a3cb6663ef465c4c2008cde21
Tyurin Andrey <ectb08@mail.ru>
2026-08-19 20:09:22 +0300

    add notification info example

--- test/app/page/ExampleBlockNotify.vue
+++ test/app/page/ExampleBlockNotify.vue
@@ -4,6 +4,7 @@
     <p>Notification examples</p>
 
     <ul class="icon-list">
+      <li><a href="#" @click.prevent="showNotificationInfo">Show notification info</a></li>
       <li><a href="#" @click.prevent="showNotificationSuccess">Show notification success</a></li>
       <li><a href="#" @click.prevent="showNotificationWarning">Show notification warning</a></li>
       <li><a href="#" @click.prevent="showNotificationError">Show notification error</a></li>
@@ -17,25 +18,25 @@ export default {
   name: 'ExampleBlockNotify',
 
   methods: {
+    showNotificationInfo() {
+      this.$dialog.Notification.info('Запущено', 'Обработка начата');
+    },
     showNotificationSuccess() {
-      this.$dialog.Notification.success('Успешно', 'Запись добавлена')
+      this.$dialog.Notification.success('Успешно', 'Запись добавлена');
     },
 
     showNotificationWarning() {
-      this.$dialog.Notification.warning(
-        'Предупреждение',
-        'Данный сервис не доступен, попробуйте через 5 минут',
-      )
+      this.$dialog.Notification.warning('Предупреждение', 'Данный сервис не доступен, попробуйте через 5 минут');
     },
 
     showNotificationError() {
-      this.$dialog.Notification.error('Ошибка сервера', 'Произошла не известная ошибка')
+      this.$dialog.Notification.error('Ошибка сервера', 'Произошла не известная ошибка');
     },
 
     showNotificationCount3() {
-      this.showNotificationSuccess()
-      this.showNotificationWarning()
-      this.showNotificationError()
+      this.showNotificationSuccess();
+      this.showNotificationWarning();
+      this.showNotificationError();
     },
   },
 }
