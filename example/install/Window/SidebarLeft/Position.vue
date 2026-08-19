273c79192483fe14397fe100b3ef445c917b1dd6
oploshka <43810171+oploshka@users.noreply.github.com>
2026-08-19 12:28:27 +0300

    Add SidebarLeft position

---
+++ example/install/Window/SidebarLeft/Position.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="sidebar-left-position">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'SidebarLeftPosition',
+})
+</script>
+
+<style scoped lang="scss">
+.sidebar-left-position {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  width: 80vw;
+}
+
+@media (min-width: 768px) {
+  .sidebar-left-position {
+    width: 240px;
+  }
+}
+</style>
