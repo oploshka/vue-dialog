25d85c57b119745977fe2ca82c49fa612a2607c2
oploshka <43810171+oploshka@users.noreply.github.com>
2026-08-19 16:04:31 +0300

    Add window facade showcase

--- test/app/page/ExampleBlockModal.vue
+++ test/app/page/ExampleBlockModal.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
-    <h2>Modal</h2>
-    <p>Modal example</p>
+    <h2>Window</h2>
+    <p>Window facade examples</p>
+
     <ul class="icon-list">
-      <li><a href="#" @click.stop="showModal">Show modal</a></li>
+      <li><a href="#" @click.prevent="showModal">Show modal</a></li>
+      <li><a href="#" @click.prevent="showFullscreen">Show fullscreen</a></li>
+      <li><a href="#" @click.prevent="showSidebarLeft">Show sidebar left</a></li>
+      <li><a href="#" @click.prevent="showSidebarRight">Show sidebar right</a></li>
     </ul>
   </div>
 </template>
@@ -17,8 +21,26 @@ export default {
 
   methods: {
     showModal() {
-      const props = reactive({ version: '213' })
+      const props = reactive({ version: 'Modal', companyName: 'Centered window' })
       const modal = this.$dialog.Modal.open(ExampleBlockModalAbout, props)
+      console.log(modal, props)
+    },
+
+    showFullscreen() {
+      const props = reactive({ version: 'Fullscreen', companyName: 'Fullscreen window' })
+      const modal = this.$dialog.Fullscreen.open(ExampleBlockModalAbout, props)
+      console.log(modal, props)
+    },
+
+    showSidebarLeft() {
+      const props = reactive({ version: 'SidebarLeft', companyName: 'Left sidebar' })
+      const modal = this.$dialog.SidebarLeft.open(ExampleBlockModalAbout, props)
+      console.log(modal, props)
+    },
+
+    showSidebarRight() {
+      const props = reactive({ version: 'SidebarRight', companyName: 'Right sidebar' })
+      const modal = this.$dialog.SidebarRight.open(ExampleBlockModalAbout, props)
       console.log(modal, props)
     },
   },
