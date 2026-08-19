dd7555c2ac312d5969ca2fda96300ffc0f9660e5
oploshka <43810171+oploshka@users.noreply.github.com>
2026-08-19 20:24:36 +0300

    Set default notification duration

--- example/install/layerConfig.ts
+++ example/install/layerConfig.ts
@@ -8,6 +8,7 @@ import NotificationLayout from './Window/Notification/Layout.vue'
 export const modalController = new ModalController()
 export const notificationController = new NotificationController({
   maxVisible: 3,
+  duration: 5000,
 })
 
 export const modalLayer = {
